25cd1f4210fa56c85dccf9151f818dd13d1bc253
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-03 13:38:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,44 @@
 import streamlit as st
+from database import LocalDatabase
+from business import Model
+from display import PresentationComponents
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="India GDP Forecasting Tool",
+    layout="wide",
 )
+
+pc = PresentationComponents()
+
+st.sidebar.header("Navigate the Dashboard")
+navigation = st.sidebar.radio(
+    "Dive into the Data!",
+    ["GDP Forecast :blue[Bar Chart]", "GDP Forecast :blue[Line Chart]", "Resilient Economy", "Model Performance", "Download Forecasted Data"],
+    captions=[
+        "Visual Representation",
+        "Visual Representation.",
+        "Visual Representation",
+        "Visual Representation",
+        "CSV file"
+        
+    ],
+)
+st.sidebar.header("Neural Edge AI")
+
+# Home section
+if navigation == "GDP Forecast :blue[Bar Chart]":
+    st.subheader("GDP Forecast :blue[Bar Chart]")
+    years = st.slider("Forecast Horizon (number of years)", 0, 20, 7)
+    pc.forecast_bar_chart(years = years+1)
+elif navigation == "GDP Forecast :blue[Line Chart]":
+    st.subheader("GDP Forecast :blue[Line Chart]")
+    years = st.slider("Forecast Horizon", 0, 20, 7)
+    pc.forecast_line_chart(years = years+1)
+elif navigation == "Model Performance":
+    pc.modelPerformance()
+elif navigation == "Download Forecasted Data":
+    years = st.slider("Forecast Horizon", 0, 20, 7)
+    pc.downloadData(years = years+1)
+elif navigation == "Resilient Economy":
+    pc.resilientEconomy()
+    
