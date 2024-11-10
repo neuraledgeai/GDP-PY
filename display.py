b4f0e6a892ec76e1080807a342d20db847355ccf
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-10 20:13:08 +0530

    Update display.py

--- display.py
+++ display.py
@@ -97,7 +97,7 @@ class PresentationComponents:
     )
     
     st.plotly_chart(fig)
-    st.write(f"The Bar Chart provides a visual representation of India’s projected GDP growth over the next {years-1} years. Each bar represents the forecasted GDP value for a given year, displayed in trillions of US dollars.")
+    st.write(f"The bar chart provides a visual representation of India’s projected GDP growth over the next {years-1} years. Each bar represents the forecasted GDP value for a given year, displayed in trillions of US dollars.")
     st.markdown("""
     <div style="text-align: center; font-size: 0.8em; color: grey;">
     The model is trained on data only up to 2023, so predictions may vary due to future uncertainties. Please verify important information independently.
