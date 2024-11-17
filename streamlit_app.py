5eca25403a70cd1d4195a10d597d10bb8a5eb115
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-17 14:34:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.sidebar.header("Neural Edge AI")
 
 # Dashboard
 if navigation == "GDP Forecast":
-    st.subheader(":blue[When will India touch $5 trillion economy milestone?]")
+    st.subheader(":blue[When will India Touch $5 Trillion Economy Milestone?]")
     years = st.slider("Forecast Horizon (number of years)", 0, 20, 7)
     st.write("You can adjust the forecast horizon to see the predicted GDPs over the next few years.")
     pc.forecast_primary_chart(years = years+1)
