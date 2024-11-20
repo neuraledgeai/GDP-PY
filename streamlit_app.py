5d2501b2115a8f831b35c1e1371fdb11a8ca78f9
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-20 16:38:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ pc = PresentationComponents()
 st.sidebar.header(":rainbow[GDP-PY]")
 navigation = st.sidebar.radio(
     "",
-    ["GDP Forecast", "*Resilient Economy*", "Emperical Results", "Download Forecasted Data"],
+    ["*GDP Forecast*", "*Resilient Economy*", "*Emperical Results*", "*Download Forecasted Data*"],
     captions=[
         "Visual Representation",
         "Visual Representation",
@@ -26,16 +26,16 @@ navigation = st.sidebar.radio(
 st.sidebar.header("Neural Edge AI")
 
 # Dashboard
-if navigation == "GDP Forecast":
+if navigation == "*GDP Forecast*":
     st.subheader(":blue[When will India Touch $5 Trillion Economy Milestone?]")
     st.write("GDP-PY explores India's journey towards becoming a $5 trillion economy. You can start by adjusting the *forecast horizon* to see the predicted GDPs over the next few years.")
     years = st.slider("Forecast Horizon (number of years)", 0, 20, 7)
     pc.forecast_primary_chart(years = years+1)
     pc.forecast_bar_chart(years = years+1)
-elif navigation == "Emperical Results":
+elif navigation == "*Emperical Results*":
     pc.empericalResults()
-elif navigation == "Download Forecasted Data":
+elif navigation == "*Download Forecasted Data*":
     years = st.slider("Forecast Horizon", 0, 20, 7)
     pc.downloadData(years = years+1)
-elif navigation == "Resilient Economy":
+elif navigation == "*Resilient Economy*":
     pc.resilientEconomy()
