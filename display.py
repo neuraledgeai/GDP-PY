3e5645104b63276044a5009287b6341a5beb51fc
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-23 21:39:33 +0530

    Update display.py

--- display.py
+++ display.py
@@ -61,8 +61,9 @@ class PresentationComponents:
       st.info("Adjust the *forecast horizon* to see the predicted GDPs over the next few years.", icon="💡")
        
     st.plotly_chart(fig)
-    st.write("The line chart illustrates India's GDP trajectory from 1960 to 2023, showcasing the nation's impressive economic rise. The **blue line** represents the actual GDP values recorded over time. The **red dashed line** represents the forecasted GDP from 2024 onward, offering insights into potential economic expansion in the coming years. (You can click or touch on the lines for detailed data points).")
-  
+    st.write("The line chart illustrates India's GDP trajectory at current prices from 1960 to 2023, showcasing the nation's impressive economic rise.")
+    st.write("The **blue line** represents the actual GDP values recorded over time. The **red dashed line** represents the forecasted GDP from 2024 onward, offering insights into potential economic expansion in the coming years. (You can click or touch on the lines for detailed data points)")
+    
   def forecast_bar_chart(self,years):
     # Get dataframe
     df =  self.model.makeForecast(years = range(1, years))
